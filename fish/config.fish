431ecb411ed2377017daea77b8e32e352118a1a5
Steven Ametjan <steve@hittingthebottle.com>
2021-05-29 11:36:12 -0700

    Begin configuring Fish (#1)

--- fish/config.fish
+++ fish/config.fish
@@ -1,2 +1,8 @@
+# Make sure brew is is on PATH
+eval (/opt/homebrew/bin/brew shellenv)
 
+# Setup ASDF for programming languages
 source /opt/homebrew/opt/asdf/asdf.fish
+
+# Source autojump
+[ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
