5e554073c6ff5f4acd945c2c5c89fc97c612d399
Steven Ametjan <sametjan@Blaster.socal.rr.com>
2021-09-01 09:21:54 -0700

    Add pipx to Fish

--- fish/config.fish
+++ fish/config.fish
@@ -6,3 +6,5 @@ source /opt/homebrew/opt/asdf/asdf.fish
 
 # Source autojump
 [ -f /opt/homebrew/share/autojump/autojump.fish ]; and source /opt/homebrew/share/autojump/autojump.fish
+# Created by `pipx` on 2021-07-24 12:58:23
+set PATH $PATH /Users/sametjan/.local/bin
